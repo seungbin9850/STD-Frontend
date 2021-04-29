5983942eb95ddd282f1d46ae86f86f9a9bd23979
senugbin9850 <tanja1206@naver.com>
2021-04-29 22:25:32 +0900

    [ADD] Signup component

--- src/components/Signup/signup.js
+++ src/components/Signup/signup.js
@@ -1,18 +1,73 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useHistory } from "react-router";
+import { signup } from "../../api";
 import * as S from "./style";
 
 const Signup = () => {
   const [id, setId] = useState("");
   const [password, setPassowrd] = useState("");
   const [nickname, setNickname] = useState("");
+  const history = useHistory();
+
+  const idInputOnChange = useCallback((e) => setId(e.target.value), []);
+  const passwordInputOnChange = useCallback(
+    (e) => setPassowrd(e.target.value),
+    []
+  );
+  const nicknameInputOnChange = useCallback(
+    (e) => setNickname(e.target.value),
+    []
+  );
+  const signupButtonClickHandler = useCallback(async () => {
+    try {
+      await signup(id, password, nickname);
+      alert("회원가입 성공");
+      history.push("/");
+    } catch (e) {
+      switch (e.response.status) {
+        case 400:
+          alert("형식이 잘못됨");
+          break;
+        case 409:
+          alert("유저가 이미 존재함");
+          break;
+        case 500:
+          alert("에러");
+          break;
+      }
+    }
+  }, [id, password, nickname]);
+  const pressHandler = useCallback(
+    (e) => {
+      if (e.keyCode === 13) {
+        signupButtonClickHandler();
+      }
+    },
+    [signupButtonClickHandler]
+  );
 
   return (
     <S.SignupContainer>
       <S.SignupSection>
-        <S.SignupInput placeholder="아이디" />
-        <S.SignupInput placeholder="비밀번호" type="password" />
-        <S.SignupInput placeholder="닉네임" />
-        <S.SignupButton>Sign Up</S.SignupButton>
+        <S.SignupInput
+          placeholder="아이디"
+          onChange={idInputOnChange}
+          onKeyDown={pressHandler}
+        />
+        <S.SignupInput
+          placeholder="비밀번호"
+          type="password"
+          onChange={passwordInputOnChange}
+          onKeyDown={pressHandler}
+        />
+        <S.SignupInput
+          placeholder="닉네임"
+          onChange={nicknameInputOnChange}
+          onKeyDown={pressHandler}
+        />
+        <S.SignupButton onClick={signupButtonClickHandler}>
+          Sign Up
+        </S.SignupButton>
         <S.MoveTextContainer>
           <S.MoveText to="/">로그인</S.MoveText>
           <S.MoveText>비밀번호 찾기</S.MoveText>
