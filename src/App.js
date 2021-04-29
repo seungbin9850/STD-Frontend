d5c4f9cb59b6cadd545b1bc48bc029720cb936dd
senugbin9850 <tanja1206@naver.com>
2021-04-29 17:10:59 +0900

    [ADD] signup router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
 
 function App() {
   return (
     <>
       <Router>
         <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={Signup} />
       </Router>
     </>
   );
