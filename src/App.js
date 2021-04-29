0275348374351368dd56d8b3cb55bbaf5ad08653
senugbin9850 <tanja1206@naver.com>
2021-04-29 15:24:45 +0900

    [ADD] Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Login } from "./components/Login";
 
 function App() {
-  return <Login></Login>;
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={Login} />
+      </Router>
+    </>
+  );
 }
 
 export default App;
