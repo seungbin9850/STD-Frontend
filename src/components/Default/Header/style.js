c7fac35ed680d2f43af27e35fc9512e4a40c941b
senugbin9850 <tanja1206@naver.com>
2021-05-11 09:08:07 +0900

    [ADD] Header style

---
+++ src/components/Default/Header/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  border-bottom: 1px solid rgba(112, 112, 112, 0.5);
+  justify-content: flex-end;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const HeaderText = styled.button`
+  all: unset;
+  margin-right: 80px;
+  font-size: 20px;
+  font-family: "NanumGothicBold";
+  :focus {
+    cursor: pointer;
+  }
+`;
