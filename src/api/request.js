07a4c3ce114d299eed70b9086682fd8fc45ce188
senugbin9850 <tanja1206@naver.com>
2021-05-12 19:49:51 +0900

    [ADD] request with access token

--- src/api/request.js
+++ src/api/request.js
@@ -7,3 +7,11 @@ export const apiRequest = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+export const apiRequestWithAccessToken = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+});
