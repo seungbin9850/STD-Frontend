cae313c574e1b8c7a6f2d3e4de102a344b10f590
senugbin9850 <tanja1206@naver.com>
2021-05-12 19:50:00 +0900

    [ADD] routing main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Login } from "./components/Login";
+import { FindMain, MakeMain, MyMain } from "./components/Main";
 import { Signup } from "./components/Signup";
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
       <Router>
         <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/find" component={FindMain} />
+        <Route exact path="/mine" component={MyMain} />
+        <Route exact path="/make" component={MakeMain} />
       </Router>
     </>
   );
