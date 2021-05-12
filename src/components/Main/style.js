3d16a064a2ee08e89de3d12602a6377c3762a1fc
senugbin9850 <tanja1206@naver.com>
2021-05-12 19:53:18 +0900

    [ADD] Main component style

---
+++ src/components/Main/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const MainBody = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainContainer = styled.div`
+  flex: 1;
+  background-color: rgba(199, 199, 199, 0.16);
+  display: flex;
+`;
