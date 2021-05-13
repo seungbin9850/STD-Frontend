741f80abcfd6ca8df72646de07124f77e3e39dae
senugbin9850 <tanja1206@naver.com>
2021-05-13 09:47:15 +0900

    [ADD] search posts request

--- src/api/post.js
+++ src/api/post.js
@@ -3,3 +3,7 @@ import { apiRequestWithAccessToken } from "./request";
 export const getPosts = (page) => {
   return apiRequestWithAccessToken.get(`/posts?page=${page}`);
 };
+
+export const searchPosts = (tag, page) => {
+  return apiRequestWithAccessToken.get(`/posts?page=${page}&tag=${tag}`);
+};
